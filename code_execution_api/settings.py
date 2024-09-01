e97ccc20d73facfc63576b025631a9518180bc7a
priyachouhan11 <priyachouhan6910@gmail.com>
2024-09-01 14:15:43 +0530

    google OAuth 2

--- code_execution_api/settings.py
+++ code_execution_api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-f=y44bdep!5r^n(=ky0rac&=41r^k_w$94yshsr5mrqey_twy%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
